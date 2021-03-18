a65649e3105dc96abd7f068e131ac10e378b6c79
Pawel <pawel.krzesinski@oneserve.co.uk>
2021-03-17 20:47:21 -0700

    Working on adding small value gems to the same bag. Also added possibly a new method of solving this problem

--- app1.js
+++ app1.js
@@ -1,36 +1,28 @@
 function splitTreasure(treasure){
+	//initialize a separate chest that will store all the shares
+	const treasureDivided = [];
 	//If all the gems in the bag are the same, you can split it between treasure.length of seekers
-	if(treasure.every( (val, item, sortedTreasure) => val === sortedTreasure[0]) ){
+	if(treasure.every((val, item, treasure) => val === treasure[0])){
+		treasure.forEach(element => {
+			treasureDivided.push([Number(element)])
+		});
+		console.log(treasureDivided);
 		console.log(`All the gems in the chest are the same, so you can split it between ${treasure.length} seekers!`);
+		return treasureDivided;
 	} else {
 	// sort the gems from biggest to the lowest
 	const sortedTreasure = treasure.sort((a, b) => b - a);
-	console.log(sortedTreasure);
 	//set first gem (biggest) in the bag as a match case and the rest as the treasure that needs dividing
 	const [firstGem, ...treasureToBeDivided] = sortedTreasure;
-	//initialize a separate chest that will store all the shares
-	const treasureDivided = [];
 	//push the first share in to the chest
 	treasureDivided.push([firstGem]);
-	//check for any duplicates, if found, push them in to main array
-	console.log(treasureToBeDivided);	
-	let count = 0;
-	treasureToBeDivided.forEach((element, index) => {
-		if(element === treasureToBeDivided[0]){
-			console.log(`Pushing gem of value ${element} to the main array`);
-			treasureDivided.push([element]);
-			//treasureToBeDivided.splice(index, 1);
-			count++;
-			return count;
-		}
-	});
-	console.log(count);
-	treasureToBeDivided.splice(0, count);
-	
 	//await addShare function to return a full bag
+	console.log("Treasure left to divide",treasureToBeDivided);
+	console.log("Treasure that has been divided",treasureDivided);
 	addShare(treasureToBeDivided, treasureDivided);
 	console.log(treasureDivided);
 	console.log(`The treasure has been shared equally between ${treasureDivided.length} seekers`);
+	return treasureDivided;
 }}
 
 function addShare(treasureToBeDivided, treasureDivided){
@@ -43,14 +35,25 @@ function addShare(treasureToBeDivided, treasureDivided){
 	if(treasureToBeDivided.length > 0){
 		addShare(treasureToBeDivided, treasureDivided);
 	} else {
-		console.log("Treasure fully divided wohoo");
+		console.log("Treasure has been divided");
 	}
 }
 
 function addGem(treasureToBeDivided, treasureDivided, pirateBag){
 	//add a gem to the pirateBag
-	pirateBag.push(treasureToBeDivided[0])
-	treasureToBeDivided.splice(0, 1)
+	
+	// if pirateBag.length not bigger than 0
+	if(pirateBag.length === 0){
+		pirateBag.push(treasureToBeDivided[0])
+		treasureToBeDivided.splice(0, 1)
+		checkIfGemCanBeAdded(treasureToBeDivided, treasureDivided, pirateBag);
+	} else {
+		checkIfGemCanBeAdded(treasureToBeDivided, treasureDivided, pirateBag);
+	}
+	return pirateBag;
+}
+
+function checkIfGemCanBeAdded(treasureToBeDivided, treasureDivided, pirateBag){
 	console.log(`Pirate bag contains: ${pirateBag}`);
 	// check the value of the pirate bag
 	let pirateBagValue = pirateBag.reduce((a, b) => { Number(a) + Number(b)})
@@ -58,36 +61,47 @@ function addGem(treasureToBeDivided, treasureDivided, pirateBag){
 	// if the bag doesn't equal first share, add a gem, otherwise return a bag
 	if(!(pirateBagValue >= treasureDivided[0][0])){
 		for(let index = treasureToBeDivided.length - 1; index >= 0; index--){
-			if(pirateBagValue + treasureToBeDivided[index] <= treasureDivided[0][0]){
+			console.log("haha");
+			if(pirateBagValue + treasureToBeDivided[index] === treasureDivided[0][0]){
 				console.log(`Found a gem, pushing ${treasureToBeDivided[index]} to a Pirate bag containing ${pirateBag}`);
 				pirateBag.push(treasureToBeDivided[index]);
 				console.log(`Removing used gem with value of ${treasureToBeDivided[index]} from the array`);
 				treasureToBeDivided.splice(index, 1);
-				let bagTotal = pirateBag.reduce((a, b) =>  Number(a) + Number(b));
-				console.log(`Bag total: ${bagTotal}`);
-				if(bagTotal === treasureDivided[0][0]){
-					console.log(`Sending a tresure bag with ${pirateBag} over to the main function!`);
-					return pirateBag;	
-				} else if(!(bagTotal + treasureToBeDivided[index] > treasureDivided[0][0])){
+				pirateBagValue = pirateBag.reduce((a, b) =>  Number(a) + Number(b));
+				console.log(`Bag total: ${pirateBagValue}`);
+				
+				if(pirateBagValue === treasureDivided[0][0]){
+					console.log(`Sending a treasure bag with ${pirateBag} over to the main function!`);
+				return pirateBag;	
+				} else if(!(pirateBagValue + treasureToBeDivided[index] > treasureDivided[0][0])){
 					console.log("Not enough! Trying to add another gem...");
 					addGem(treasureToBeDivided, treasureDivided, pirateBag);
-				}				
-			} 
-			// else if(index > -1) {
-			// 		addGem(treasureToBeDivided, treasureDivided, pirateBag)
-			// 		console.log("haha");
-			//} 
-			else {
-				console.log("Can't find a match! Can't equally share this treasure! Stopping...");
-				return [];
-			}	
+				}
+				// if(pirateBagValue + treasureToBeDivided[index] < treasureDivided[0][0]){
+				// 	pirateBag.push(treasureToBeDivided[index])
+				// 	treasureToBeDivided.splice(index, 1);
+					
+				// }
+
+
+			}
+			
 		}
+	} else {
+		return pirateBag;
 	}
 }
 
+//splitTreasure([6,3,2,4,1])
+splitTreasure([3,2,7,7,14,5,3,4,9,2]);
+//splitTreasure([2, 10, 2, 4, 2, 9, 3, 5, 1, 2])
+//splitTreasure([4,4,4])
+//splitTreasure([27,7,20]) 
+//splitTreasure([6,3,2,4,1] )
+//splitTreasure([3,2,7,7,14,5,3,4,9,2])
+// [[14], [9, 5], [7, 7], [4, 3, 3, 2, 2]]
 
-splitTreasure([27, 7, 20, 13, 14, 27, 27, 7, 20, 13, 14, 27]);
-//[2, 10, 2, 4, 2, 9, 3, 5, 1, 2]
+
 module.exports = {
 	splitTreasure: splitTreasure
 }
--- myTests.test.js
+++ myTests.test.js
@@ -3,18 +3,25 @@ const treasureSplittingApp = require('./app1')
 
 test("When all gems are same value splits evenly", () => {
 	const result = treasureSplittingApp.splitTreasure([4,4,4]);
+	console.log(`The result is ${result}`);
 	expect(result.length).toEqual(3);
 	expect(result[0]).toContain(4);
 	expect(result[1]).toContain(4);
 	expect(result[2]).toContain(4);
 });
 
-test("splitTreasure [27,7,20]", () => {
-
+test("splitTreasure 27,7,20", () => {
 	const result = treasureSplittingApp.splitTreasure([27,7,20]);
+	console.log(result);
 	expect(result.length).toEqual(2);
-	expect(result[0]).toContain([27]);
-	expect(result[1]).toContain([20, 7]);
-
+	expect(result[0]).toContain(27);
+	expect(result[1]).toContain(20, 7);
 })
 
+test("splitTreasure 27,7,20", () => {
+	const result = treasureSplittingApp.splitTreasure([27,7,20]);
+	console.log(result);
+	expect(result.length).toEqual(2);
+	expect(result[0]).toContain(27);
+	expect(result[1]).toContain(20, 7);
+})
